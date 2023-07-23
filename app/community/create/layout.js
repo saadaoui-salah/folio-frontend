76e568e296e9178a6a39dbcc50862acfd3cb8d46
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-07-23 09:45:42 -0400

    :ladybeetle:

--- app/community/create/layout.js
+++ app/community/create/layout.js
@@ -1,13 +1,8 @@
-import "./globals.css";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
-
 export const metadata = {
   title: "Folio",
   description: "share your portofolio",
 };
 
 export default function RootLayout({ children }) {
-  return { children };
+  return children;
 }
