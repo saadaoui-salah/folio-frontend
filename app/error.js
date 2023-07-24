558f5b0dcd8a8ac2378932d8242f3a5981fb62e7
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-07-23 21:55:54 -0400

    :heavy_check_mark: create error page

--- app/error.js
+++ app/error.js
@@ -1,5 +1,27 @@
 "use client";
+import Link from "next/link";
 
-export default function CustomError() {
-  return <p>error</p>;
+export default function CustomError({ error, reset }) {
+  return (
+    <section className="w-full bg-blue-50 flex flex-col items-center h-[99vh] justify-center">
+      <h1 className="text-3xl font-semibold mb-4">
+        Something went wrong ... 😢
+      </h1>
+      <p>{error.message}</p>
+      <div className="flex w-[280px] justify-between">
+        <button
+          onClick={() => reset()}
+          className="border border-blue-500 bg-blue-500 text-white font-bold px-6 py-[9px] rounded-md mt-4"
+        >
+          Try again
+        </button>
+        <Link
+          href={"/"}
+          className="border border-blue-500 hover:bg-blue-500 text-blue-500 hover:text-white font-bold px-6 py-3 rounded-md mt-4"
+        >
+          Go Home
+        </Link>
+      </div>
+    </section>
+  );
 }
