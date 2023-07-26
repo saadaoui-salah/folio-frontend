751e870be3c9fe748cf396ddfb9bfe182ee9663a
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-07-25 21:26:28 -0400

    :trash; delete unused button

--- components/Buttons.js
+++ components/Buttons.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { CloseMenuIcon, NotificationIcon, OpenMenuIcon } from "./Icons";
+import { NotificationIcon } from "./Icons";
 import Image from "next/image";
 
 export const ProfileButoon = ({ action }) => {
@@ -33,23 +33,6 @@ export const NotificationButton = ({ action }) => {
       </span>
       <NotificationIcon />
     </button>
-  );
-};
-
-export const MobileMenuButtons = ({ onclick, open }) => {
-  return (
-    <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-      <button
-        onClick={() => onclick()}
-        type="button"
-        id="dropdownBtn"
-        className="inline-flex items-center justify-center rounded-md p-2 hover:bg-gray-200 text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-        aria-controls="mobile-menu"
-        aria-expanded="false"
-      >
-        {open ? <OpenMenuIcon /> : <CloseMenuIcon />}
-      </button>
-    </div>
   );
 };
 
