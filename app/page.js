eb742ec35d76e9a5c7760c7fc9c95fb6b33e1388
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-07-25 21:25:13 -0400

    :heavy_minus_sign: delete margin top

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import { PostSection, Suggestions } from "@/components";
 
 export default function Home() {
   return (
-    <div className="mt-10 flex items-start justify-evenly">
+    <div className="flex items-start justify-evenly">
       <Suggestions title={"Suggested Peoples"} />
       <PostSection />
       <Suggestions title={"Suggested Comunities"} />
