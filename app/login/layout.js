5990f6ad254c35b6e4692d5d6c2de1892218faec
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-07-25 21:25:40 -0400

    :lady_beetle:

--- app/login/layout.js
+++ app/login/layout.js
@@ -1,13 +1,8 @@
-import "./globals.css";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
-
 export const metadata = {
   title: "Login - Folio",
   description: "login to folio",
 };
 
 export default function RootLayout({ children }) {
-  return { children };
+  return children;
 }
--- app/support/layout.js
+++ app/support/layout.js
@@ -1,17 +1,8 @@
-import "./globals.css";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
-
 export const metadata = {
   title: "Support - Folio",
   description: "contact folio support ",
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+  return children;
 }
