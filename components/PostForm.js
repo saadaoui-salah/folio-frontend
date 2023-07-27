9a20c2dc88505395b71eb6f4cea4b8b1baaf4db7
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-07-27 15:29:37 -0400

    :recycle: width & height of post form

--- components/PostForm.js
+++ components/PostForm.js
@@ -3,14 +3,14 @@ import { XIcon } from "./Icons";
 
 const PostForm = () => {
   return (
-    <form className="md:w-[500px] bg-white w-[400px] border border-slate-300 rounded-md">
-      <div className="p-2 flex items-center gap-4 border-b border-slate-400">
+    <form className="md:w-[500px] lg:w-[700px] bg-white w-[400px] border border-slate-300 rounded-md">
+      <div className="p-2 flex items-center gap-4 border-b border-slate-300">
         <img className="bg-slate-400 w-10 h-10 rounded-full" />
         <p className="font-bold">User Name</p>
       </div>
       <textarea
         placeholder="Share post ..."
-        className="p-2 w-full border-b h-28 border-slate-400 bg-transparent"
+        className="p-2 w-full border-b h-36 border-slate-300 bg-transparent"
       ></textarea>
       <div className="px-3 py-2 flex gap-4 flex-wrap">
         <div className="relative">
@@ -20,7 +20,7 @@ const PostForm = () => {
           </button>
         </div>
       </div>
-      <div className="flex border-t border-slate-400 items-center justify-between px-2 py-1">
+      <div className="flex border-t border-slate-300 items-center justify-between px-2 py-1">
         <button className="px-2 py-1">Upload</button>
         <button className="px-2 py-1">share</button>
       </div>
