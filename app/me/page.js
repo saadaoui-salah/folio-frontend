f775835c5458a8def57b8ea32d30cf4a9126a1d0
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-07-27 16:23:41 -0400

    :dart: use gap instead of justify-evenly

--- app/me/page.js
+++ app/me/page.js
@@ -4,7 +4,7 @@ export default function Me() {
   return (
     <>
       <Info />
-      <div className="flex justify-evenly flex-wrap mt-16">
+      <div className="flex justify-center gap-16 flex-wrap mt-16">
         <div className=" px-4">
           <GroupOfCards title="Followers" link="/followers" />
           <GroupOfCards title="Projects" link="/projects" />
--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import { PostSection, Suggestions } from "@/components";
 
 export default function Home() {
   return (
-    <div className="flex items-start justify-evenly">
+    <div className="flex items-start justify-center gap-8">
       <Suggestions title={"Suggested Peoples"} />
       <PostSection />
       <Suggestions title={"Suggested Comunities"} />
