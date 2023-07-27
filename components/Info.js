65345d94da5a308ec9b2f50f0de760f117f1425f
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-07-27 19:09:49 -0400

    :lady_beetle: fix profile image position

--- components/Info.js
+++ components/Info.js
@@ -11,24 +11,26 @@ const Info = () => {
   };
   return (
     <section>
-      <div className="pb-6 border-b-2 w-full flex flex-col items-center px-6 justify-center">
-        <img className="w-[800px] h-[250px] max-sm:h-[180px] bg-red-200 rounded-md" />
-        <div className="flex items-center justify-start w-full px-8">
-          <img className="w-[110px] h-[110px] mt-[-40px] rounded-full max-sm:h-[100px] bg-green-200 mx-6" />
-          <div className="flex items-center gap-6">
-            {edit ? (
-              <input
-                className="bg-transparent text-[24px] w-[150px] border rounded-md px-2 border-blue-600"
-                value={fullName}
-                onChange={(e) => setFullName(e.target.value)}
-              />
-            ) : (
-              <h1 className="text-[24px] px-2">{fullName}</h1>
-            )}
-            <div
-              onClick={handleEdit}
-              className="w-8 h-8 cursor-pointer bg-slate-300 rounded-full"
-            ></div>
+      <div className="pb-6 relative border-b-2 w-full flex flex-col items-center px-6 max-sm:p-0 justify-center">
+        <div className="relative w-[800px] max-sm:w-full h-[350px] max-sm:h-[280px]">
+          <img className="w-[800px] max-sm:w-full h-[250px] max-sm:h-[180px] bg-red-200 rounded-md" />
+          <div className="absolute flex items-center justify-start w-full px-8">
+            <img className="w-[110px] h-[110px] mt-[-40px] rounded-full max-sm:h-[100px] bg-green-200 mx-6" />
+            <div className="flex items-center gap-6">
+              {edit ? (
+                <input
+                  className="bg-transparent text-[24px] w-[150px] border rounded-md px-2 border-blue-600"
+                  value={fullName}
+                  onChange={(e) => setFullName(e.target.value)}
+                />
+              ) : (
+                <h1 className="text-[24px] px-2">{fullName}</h1>
+              )}
+              <div
+                onClick={handleEdit}
+                className="w-8 h-8 cursor-pointer bg-slate-300 rounded-full"
+              ></div>
+            </div>
           </div>
         </div>
       </div>
